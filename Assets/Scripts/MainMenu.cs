875a7ffd8fafdff53f24fbea09a9658f21d136c8
Jenni <medfouedjenni@gmail.com>
2021-08-12 11:19:29 +0100

    Toggle blind mode

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
     // Start the game when Play button is pressed from the main menu
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
         SceneManager.LoadScene("Maze");
     }
 
